5511ffb259ca66c405dacabae4f184abd5e3c1b7
USMAN-PRO-TECH <usmannaeem52666@gmail.com>
2024-11-08 17:18:11 +0500

    new design added

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 import mdx from '@astrojs/mdx';
 import tailwindcss from '@tailwindcss/vite';
+import react from '@astrojs/react';
 export default defineConfig({
 vite: {
     plugins: [tailwindcss()],
   },
     // add yur domain name here
    site: 'https://callsupport.ai',
-  integrations: [sitemap(),mdx()]
+  integrations: [sitemap(),mdx(), react()]
 });
