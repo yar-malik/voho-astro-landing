cadc93061b30b42c50e0d2f3f582bd236c03071e
USMAN-PRO-TECH <usmannaeem52666@gmail.com>
2024-11-10 16:55:38 +0500

    testing live  demo

--- src/components/vapi/Button.jsx
+++
@@ -1,9 +0,0 @@
-const Button = ({ children, onClick, type = 'button', className = '', disabled = false, width }) => {
-    return (
-        <button onClick={onClick} type={type} className={className} disabled={disabled} style={{ width }}>
-            {children}
-        </button>
-    );
-};
-
-export default Button;
