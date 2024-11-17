42decd46103c86d9b0a8c0a4015e7a779fd84c84
USMAN-PRO-TECH <usmannaeem52666@gmail.com>
2024-11-17 18:51:52 +0500

    demo call and enterprise form fuctionallity added

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,12 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+interface ImportMetaEnv {
+    readonly PUBLIC_VAPI_PUBLIC_KEY: string;
+    readonly SECRET_PASSWORD_PIVATE_KEY: string;
+    readonly PUBLIC_VAPI_ASSISTANT_ID: string;
+    readonly SECRET_PASSWORD_PHONE_KEY: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
